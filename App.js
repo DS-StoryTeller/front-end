656f19fc81202720d1e249dbc319d78ecf735a3d
dona <ehsk0123@naver.com>
2024-05-10 22:48:07 +0900

    [Fix] Render 오류 수정

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Login from './src/screens/Login';
 import Signin from './src/screens/Signin';
 import BookShelf from './src/screens/BookShelf';
 import BookRead from './src/screens/BookRead';
+import Quiz from './src/screens/Quiz';
 import Profile from './src/screens/Profile';
 
 const App = () => {
-    const Stack =createNativeStackNavigator();
-   
+    const Stack = createNativeStackNavigator();
+
     return (
         <NavigationContainer>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -21,7 +23,7 @@ const App = () => {
             <Stack.Screen name="Profile" component={Profile} />
           </Stack.Navigator>
         </NavigationContainer>
-      );   
-    
+      );
+
 };
 export default App;
