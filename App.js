95c75fe377d4856e8890976acf1b508263304cb7
sylvia1213 <yeayoung1213@naver.com>
2024-06-23 22:51:25 +0900

    임시 bottomTab 생성

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Login from './src/screens/Login';
 import Signin from './src/screens/Signin';
@@ -12,11 +13,32 @@ import Profile from './src/screens/Profile';
 
 const App = () => {
     const Stack = createNativeStackNavigator();
-
+    const Tab = createBottomTabNavigator();
+    const BottomTabScreen = () => {
+        return (
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarHideOnKeyboard: true,
+              tabBarShowLabel: true,
+              headerShown: false,
+              tabBarStyle: {
+                height: 30,
+              },
+            })}>
+            <Tab.Screen name="Login" component={Login} />
+            <Tab.Screen name="Signin" component={Signin} />
+            <Tab.Screen name="BookShelf" component={BookShelf} />
+            <Tab.Screen name="BookRead" component={BookRead} />
+            <Tab.Screen name="Quiz" component={Quiz} />
+            <Tab.Screen name="Profile" component={Profile} />
+          </Tab.Navigator>
+        );
+    };
   
 return (
         <NavigationContainer>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Bottom" component={BottomTabScreen} />
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Signin" component={Signin} />
             <Stack.Screen name="BookShelf" component={BookShelf} />
