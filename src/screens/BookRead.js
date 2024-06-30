9683fe0c23f7460d23677d860cc4c6f9b255d3b5
sylvia1213 <yeayoung1213@naver.com>
2024-06-30 23:16:15 +0900

    Feat: #6  전체 UI 반반으로 수정 완료

--- src/screens/BookRead.js
+++ src/screens/BookRead.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, ImageBackground, View, TouchableOpacity, Alert } from "react-native"
+import { StyleSheet, Text, ImageBackground, View, TouchableOpacity, Alert, Image } from "react-native"
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useFocusEffect } from '@react-navigation/native';
 import Page1 from '../../assets/images/page1.png';
@@ -8,11 +8,12 @@ import ProgressBar from '../components/ProgressBar';
 import SettingModal from '../components/SettingModal';
 import YesNoModal from '../components/YesNoModal';
 import NextStep from '../components/NextStep';
+import BookBg from '../../assets/images/bookBg.png';
 
 const BookRead = ({ navigation }) => {
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
-    
+
     const [isSettingModalVisible, setIsSettingModalVisible] = useState(false);
     const [isYesNoModalVisible, setIsYesNoModalVisible] = useState(false);
 
@@ -55,35 +56,37 @@ const BookRead = ({ navigation }) => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <ImageBackground source={Page1} style={styles.page}>
-
-                <View style={styles.titleBox}>
+            <ImageBackground source={BookBg} >
+                <ImageBackground source={Page1} style={styles.page}>
                     <View style={styles.iconBox}>
-                        <TouchableOpacity style={styles.icon}  onPress={openYesNoModal} >
+                        <TouchableOpacity style={styles.icon} onPress={openYesNoModal} >
                             <Ionic name="home" size={35} color="white" />
                         </TouchableOpacity>
-                        <YesNoModal isVisible={isYesNoModalVisible} onClose={closeYesNoModal} 
-                            title={"정말 중단하시겠습니까?"} 
+                        <YesNoModal isVisible={isYesNoModalVisible} onClose={closeYesNoModal}
+                            title={"정말 중단하시겠습니까?"}
                             subtitle={`다시 동화를 읽을 때 \n 현재 페이지부터 읽으실 수 있습니다.`}
                             buttonText1={"중단하기"}
                             linkTo={'BookShelf'}
-                            buttonText2={"취소"}/>
+                            buttonText2={"취소"} />
                         <TouchableOpacity style={styles.icon} onPress={openSettingModal} >
                             <Ionic name="settings" size={35} color="white" />
                         </TouchableOpacity>
                         <SettingModal isVisible={isSettingModalVisible} onClose={closeSettingModal} />
                     </View>
-                    <Text style={styles.bookTitle}>Unveiling the Enchanted Cave</Text>
-                </View>
+                </ImageBackground>
 
                 <View style={styles.textBox}>
+                    <View style={styles.titleBox}>
+                        <Text style={styles.bookTitle}>Unveiling the Enchanted Cave</Text>
+                    </View>
                     <Text style={styles.bookText}>
-                        Once upon a time, in the quaint village of Willowbrook, there lived a young girl named Eunseo. Eunseo was known for her adventurous spirit and her love for exploring the enchanted forests that surrounded her home.
+                        Once upon a time, in the quaint village of Willowbrook, there lived a young girl named Eunseo.  {"\n"}Eunseo was known for her adventurous spirit and her love for exploring the enchanted forests that surrounded her home.
                         {"\n"}One bright morning, as Eunseo ventured deeper into the woods than she had ever gone before, she stumbled upon a mysterious cave hidden beneath the thick foliage.
                     </Text>
                 </View>
-                <ProgressBar pages={'13'} now={'4'}/>
+                <ProgressBar pages={'13'} now={'4'} />
                 {nextStepVisible && <NextStep />}
+
             </ImageBackground>
         </SafeAreaView>
     )
@@ -93,51 +96,45 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#FBF7EC',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     page: {
-        width: '100%',
+        width: '70.5%',
         height: '100%',
     },
 
     titleBox: {
-        flexDirection: 'row',
+        marginBottom: 30,
+    },
+    textBox: {
+        width: '35%',
         justifyContent: 'center',
-        alignItems: 'baseline',
-        margin: 15,
+        position: 'absolute',
+        top: '5%',
+        left: '57%',
+        borderRadius: 10,
+        padding: 10,
+
     },
     bookTitle: {
         fontSize: 35,
         fontWeight: 'bold',
-        color: '#FFFFFF',
-        margin: (0, 'auto'),
+        color: 'black',
+        textAlign: 'center',
     },
     iconBox: {
         flexDirection: 'column',
         justifyContent: 'space-around',
         alignItems: 'baseline',
-        paddingLeft: 10,
+        margin: 20,
     },
     icon: {
         marginTop: 13,
     },
-    textBox: {
-        width: '50%',
-        backgroundColor: 'rgba(255, 255, 255, 0.7)',
-        justifyContent: 'center',
-        position: 'absolute',
-        top: '20%',
-        left: '45%',
-        borderRadius: 10,
-        padding: 10,
 
-    },
     bookText: {
-        fontSize: 17,
+        fontSize: 23,
         color: '#000000',
         textAlign: 'center',
-        lineHeight: 20,
     },
 
 })
