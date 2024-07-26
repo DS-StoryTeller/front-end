a3111e5393c4e27371094bf63148f133cfa5b713
dona <ehsk0123@naver.com>
2024-07-26 21:24:16 +0900

    Design: #11 프로필 수정 PIN 번호 입력 모달창 생성

---
+++ src/components/EditPinInputModal.js
@@ -0,0 +1,149 @@
+import React, {useState, useRef, useCallback} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Modal,
+} from 'react-native';
+import AddProfileModal from './AddProfileModal'; // 상대경로는 프로젝트 구조에 맞게 조정
+
+const EditPinInputModal = ({visible, onClose}) => {
+  const [pin, setPin] = useState(['', '', '', '']);
+  const [isAddProfileModalVisible, setIsAddProfileModalVisible] =
+    useState(false);
+  const inputRefs = useRef([]);
+
+  // 올바른 PIN번호
+  const correctPin = '0000';
+
+  // PIN 변경 핸들러
+  const handlePinChange = (index, value) => {
+    let newPin = [...pin];
+    newPin[index] = value;
+    setPin(newPin);
+
+    // 현재 값이 비어 있지 않고 마지막 입력이 아닌 경우 다음 입력으로 포커스 이동
+    if (value && index < pin.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+
+    // PIN이 올바른지 확인
+    if (newPin.join('') === correctPin) {
+      setIsAddProfileModalVisible(true); // AddProfileModal 표시
+      onClose(); // PinInputModal 닫기
+    }
+  };
+
+  // 모달 닫기 핸들러
+  const handleClose = useCallback(() => {
+    setPin(['', '', '', '']); // PIN 상태 초기화
+    onClose(); // 부모 컴포넌트의 onClose 호출
+  }, [onClose]);
+
+  return (
+    <>
+      <Modal
+        transparent={true}
+        animationType="slide"
+        visible={visible}
+        onRequestClose={handleClose}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <TouchableOpacity style={styles.closeButton} onPress={handleClose}>
+              <Text style={styles.closeButtonText}>X</Text>
+            </TouchableOpacity>
+            <Text style={styles.modalHeader}>이 프로필을 관리하려면 PIN 번호를 입력하세요.</Text>
+            <View style={styles.pinContainer}>
+              {pin.map((digit, index) => (
+                <TextInput
+                  key={index}
+                  ref={ref => (inputRefs.current[index] = ref)}
+                  style={styles.pinInput}
+                  keyboardType="numeric"
+                  maxLength={1}
+                  value={digit}
+                  onChangeText={value => handlePinChange(index, value)}
+                />
+              ))}
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+      {/* AddProfileModal should be conditionally rendered */}
+      {isAddProfileModalVisible && (
+        <AddProfileModal
+          visible={isAddProfileModalVisible}
+          onClose={() => setIsAddProfileModalVisible(false)}
+        />
+      )}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: '100%',
+    height: '91%',
+    backgroundColor: '#F8C784',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  closeButtonText: {
+    color: '#393939',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  modalHeader: {
+    fontSize: 35,
+    fontWeight: '900',
+    color: '#393939',
+    marginBottom: 100,
+    marginTop: -60,
+    textAlign: 'center',
+  },
+  pinContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '75%',
+    marginTop: 0,
+  },
+  pinInput: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#FCAE59',
+    borderRadius: 20,
+    fontSize: 100,
+    textAlign: 'center',
+    color: '#393939',
+    fontWeight: '900',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+});
+
+export default EditPinInputModal;
