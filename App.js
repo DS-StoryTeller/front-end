49d9226480c48c7285491fbdc43503aaa1d32e9d
dona <ehsk0123@naver.com>
2024-08-28 15:12:34 +0900

    Feat: 올바른 뒤로가기 로직을 위한 AuthProvider추가 #31

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { AuthProvider } from './src/context/AuthContext';
 
 import Login from './src/screens/Login';
 import Signin from './src/screens/Signin';
@@ -9,6 +10,7 @@ import BookRead from './src/screens/BookRead';
 import Quiz from './src/screens/Quiz';
 import Profile from './src/screens/Profile';
 import Question from './src/screens/Question';
+import QuizEnd from './src/screens/QuizEnd';
 import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
@@ -20,20 +22,22 @@ const App = () => {
     }, 300); // 0.5초 후에 스플래시 화면을 숨깁니다.
   }, []);
 
-  
-return (
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Signin" component={Signin} />
-            <Stack.Screen name="BookShelf" component={BookShelf} />
-            <Stack.Screen name="BookRead" component={BookRead} />
-            <Stack.Screen name="Quiz" component={Quiz} />
-            <Stack.Screen name="Profile" component={Profile} />
-            <Stack.Screen name="Question" component={Question} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      );
+  return (
+    <AuthProvider>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signin" component={Signin} />
+        <Stack.Screen name="BookShelf" component={BookShelf} />
+        <Stack.Screen name="BookRead" component={BookRead} />
+        <Stack.Screen name="Quiz" component={Quiz} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Question" component={Question} />
+        <Stack.Screen name="QuizEnd" component={QuizEnd} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </AuthProvider>
+  );
 };
 
 export default App;
