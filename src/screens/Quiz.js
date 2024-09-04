61cbc9c5464e390da91b54ddcbe8adb3986b5683
dona <ehsk0123@naver.com>
2024-09-04 21:51:58 +0900

    Feat: id값 퀴즈 페이지에 넘기기 #31

--- src/screens/Quiz.js
+++ src/screens/Quiz.js
@@ -1,35 +1,66 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import LinearGradient from 'react-native-linear-gradient';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 
 const Quiz = () => {
   const navigation = useNavigation();
+  const route = useRoute();
+  const {profileId, bookId} = route.params || {};
 
   const startQuiz = () => {
-    navigation.navigate('Question');
+    navigation.navigate('Question', {
+      profileId: profileId,
+      bookId: bookId,
+    });
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={require('../../assets/images/QuestionMark.png')} style={styles.questionMark} />
+      <Image
+        source={require('../../assets/images/QuestionMark.png')}
+        style={styles.questionMark}
+      />
       <View style={styles.quizBox}>
-        <Text style={styles.quizText}>잘 읽었나요?{'\n'}퀴즈를 풀어봅시다!</Text>
+        <Text style={styles.quizText}>
+          잘 읽었나요?{'\n'}퀴즈를 풀어봅시다!
+        </Text>
         <LinearGradient
           colors={['#FF8C43', '#F8C683']}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 0 }}
-          style={styles.button}
-        >
+          start={{x: 0, y: 0}}
+          end={{x: 1, y: 0}}
+          style={styles.button}>
           <TouchableOpacity style={styles.buttonContent} onPress={startQuiz}>
             <Text style={styles.buttonText}>→</Text>
           </TouchableOpacity>
         </LinearGradient>
         <View style={styles.ovalContainer}>
-          <View style={[styles.oval, { backgroundColor: '#DCDFE3', width: 60, borderRadius: 5 }]}></View>
-          <View style={[styles.oval, { backgroundColor: '#F8C784', width: 60, borderRadius: 5, marginLeft: 25 }]}></View>
-          <View style={[styles.oval, { backgroundColor: '#DCDFE3', width: 60, borderRadius: 5, marginLeft: 25 }]}></View>
+          <View
+            style={[
+              styles.oval,
+              {backgroundColor: '#DCDFE3', width: 60, borderRadius: 5},
+            ]}></View>
+          <View
+            style={[
+              styles.oval,
+              {
+                backgroundColor: '#F8C784',
+                width: 60,
+                borderRadius: 5,
+                marginLeft: 25,
+              },
+            ]}></View>
+          <View
+            style={[
+              styles.oval,
+              {
+                backgroundColor: '#DCDFE3',
+                width: 60,
+                borderRadius: 5,
+                marginLeft: 25,
+              },
+            ]}></View>
         </View>
       </View>
     </SafeAreaView>
@@ -49,7 +80,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 10,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.3,
     shadowRadius: 4,
     elevation: 5,
@@ -76,7 +107,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: -30,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.3,
     shadowRadius: 4,
     elevation: 5,
